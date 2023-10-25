e543c14af28358234aecfbbef4a38167c3865eba
brunogsr <brunogsr https://github.com/brunogsr>
2023-10-25 04:53:19 -0300

    Req 1 erro de lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,15 @@
 def study_schedule(permanence_periods, target_time):
-    if (target_time is None):
+    if target_time is None:
         return None
 
+    count = 0
     for entry in permanence_periods:
-        if (not isinstance(entry[0], int)) or (not isinstance(entry[1], int)):
+        if not all(isinstance(val, int) for val in entry) \
+                or entry[0] is None \
+                or entry[1] is None \
+                or entry[0] > entry[1]:
             return None
-        if (entry[0] is None) or (entry[1] is None):
-            continue
-        if (entry[0] > entry[1]):
-            return None
+        if entry[0] <= target_time <= entry[1]:
+            count += 1
 
-    count = sum((1 for entry in permanence_periods if (entry[0] <= target_time) and (target_time <= entry[1])))
-    return count
+    return count if count > 0 else None
